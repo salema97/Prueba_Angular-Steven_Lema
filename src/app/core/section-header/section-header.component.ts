586c0bc2f1b95123c0e6621d912283ee458bd802
salema97 <salemavelasquez97@gmail.com>
2024-04-26 15:46:03 -0500

    Add home and header components

---
+++ src/app/core/section-header/section-header.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  templateUrl: './section-header.component.html',
+})
+export class SectionHeaderComponent {}
